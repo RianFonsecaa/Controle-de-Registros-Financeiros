5d11feca761a8fa369fe6c205c1fea55106d309a
RianFonsecaa <rian.sfonsecaa@gmail.com>
2025-12-11 23:57:24 -0300

    feat: Criação do componente informativo para páginas em criação ou manutenção

---
+++ src/app/components/pagina-em-manutencao/pagina-em-manutencao.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLinkActive, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-pagina-em-manutencao',
+  imports: [RouterLink],
+  templateUrl: './pagina-em-manutencao.html',
+  styleUrl: './pagina-em-manutencao.css',
+})
+export class PaginaEmManutencao {}
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SideBar } from '../../components/side-bar/side-bar';
+import { PaginaEmManutencao } from '../../components/pagina-em-manutencao/pagina-em-manutencao';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SideBar],
+  imports: [SideBar, PaginaEmManutencao],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
