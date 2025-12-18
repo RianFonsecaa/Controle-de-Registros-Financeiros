1721e735c2155f9dba95b2dd3635833fd9cf183b
RianFonsecaa <rian.sfonsecaa@gmail.com>
2025-12-17 23:18:38 -0300

    refactor: Ajustes da lógica de controle dos modais

---
+++ src/app/components/modais/save-cobranca-modal/save-cobranca-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-cobranca-modal',
+  imports: [],
+  templateUrl: './save-cobranca-modal.html',
+  styleUrl: './save-cobranca-modal.css',
+})
+export class SaveCobrancaModal {}
--- src/app/components/tabela/tabela.ts
+++ src/app/components/tabela/tabela.ts
@@ -1,13 +1,13 @@
 import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { CobrancaResponse } from '../../model/responses/CobrancaResponse';
 import { CobrancaService } from '../../services/cobrancas.service';
-import { CurrencyPipe, DatePipe, NgFor } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { DeleteModal } from '../modais/delete-modal/delete-modal';
-import { CreateModal } from '../modais/create-modal/create-modal';
+import { SaveCobrancaModal } from '../modais/save-cobranca-modal/save-cobranca-modal';
 
 @Component({
   selector: 'app-tabela',
-  imports: [CurrencyPipe, DatePipe, DeleteModal],
+  imports: [CurrencyPipe, DatePipe, DeleteModal, SaveCobrancaModal],
   templateUrl: './tabela.html',
   styleUrl: './tabela.css',
 })
@@ -32,8 +32,10 @@ export class Tabela {
     });
   }
 
-  abrirModal(cobranca: CobrancaResponse, modal: HTMLDialogElement) {
-    this.cobrancaSelecionada = cobranca;
+  abrirModal(cobranca: CobrancaResponse | null, modal: HTMLDialogElement) {
+    if (cobranca) {
+      this.cobrancaSelecionada = cobranca;
+    }
     modal.showModal();
   }
 
